d0ab2cd22c4d860f8115aaf73b9d38995430e15d
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-20 22:57:28 +0530

    Model updated

--- master/src/main/java/com/foodFly/master/Model/Addresses.java
+++ master/src/main/java/com/foodFly/master/Model/Addresses.java
@@ -1,7 +1,17 @@
 package com.foodFly.master.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Addresses {
 
+    @Id
     private Long addressId;
 
     private String building;
