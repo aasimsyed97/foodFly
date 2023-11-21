5a910c6e84faf9602c8e2032f3dc71dc6a0985ec
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-22 00:13:05 +0530

    customer address mapping class added

---
+++ master/src/main/java/com/foodFly/master/DAO/CustomerAddressMappingDao.java
@@ -0,0 +1,13 @@
+package com.foodFly.master.DAO;
+
+import com.foodFly.master.Model.CustomerAddressMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerAddressMappingDao extends JpaRepository<CustomerAddressMapping, UUID> {
+
+
+}
