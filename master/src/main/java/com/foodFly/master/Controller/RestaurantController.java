80970f61805d799a4a5775c4c26ce2931558b468
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-24 00:45:21 +0530

    Restaurant functionality added

--- master/src/main/java/com/foodFly/master/Controller/RestaurantController.java
+++ master/src/main/java/com/foodFly/master/Controller/RestaurantController.java
@@ -1,21 +1,46 @@
 package com.foodFly.master.Controller;
 
 import com.foodFly.master.DTOs.RestaurantRequestDto;
+import com.foodFly.master.DTOs.RestaurantResponseDto;
+import com.foodFly.master.Service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestaurantController {
 
+    @Autowired
+    RestaurantService restaurantService;
 
     @PostMapping("/registerRestaurant")
-    public ResponseEntity<String> registerRestaurantController(@RequestBody RestaurantRequestDto restaurantRequestDto){
+    public ResponseEntity<RestaurantResponseDto> registerRestaurantController(@RequestBody RestaurantRequestDto restaurantRequestDto){
+
+        RestaurantResponseDto restaurantResponseDto = restaurantService.registerRestaurant(restaurantRequestDto);
+        return  new ResponseEntity<>(restaurantResponseDto, HttpStatus.CREATED);
+    }
+
+   @GetMapping("/getRestaurant")
+    public ResponseEntity<RestaurantResponseDto> getRestaurantController(@RequestParam Long restaurantId){
+
+        RestaurantResponseDto responseDto = restaurantService.getRestaurant(restaurantId);
+
+        return new ResponseEntity<>(responseDto,HttpStatus.OK);
+    }
+
+    @PutMapping("/updateRestaurant")
+    public ResponseEntity<RestaurantResponseDto> updateRestaurant(@RequestBody RestaurantRequestDto restaurantRequestDto){
+
+        RestaurantResponseDto responseDto = restaurantService.updateRestaurant(restaurantRequestDto);
+        return new ResponseEntity<>(responseDto,HttpStatus.OK);
+    }
 
 
-        return  null;
+    public ResponseEntity<String> deleteRestaurant(@RequestParam Long restaurantId){
+
+        String response = restaurantService.deleteRestaurant(restaurantId);
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
 }
