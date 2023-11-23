c890987b3b66dfe83ac97f33f0a1c9314c670816
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-23 23:58:59 +0530

    Restaurant Controller added

--- master/src/main/java/com/foodFly/master/Controller/RestaurantController.java
+++ master/src/main/java/com/foodFly/master/Controller/RestaurantController.java
@@ -1,4 +1,21 @@
 package com.foodFly.master.Controller;
 
+import com.foodFly.master.DTOs.RestaurantRequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RestaurantController {
+
+
+    @PostMapping("/registerRestaurant")
+    public ResponseEntity<String> registerRestaurantController(@RequestBody RestaurantRequestDto restaurantRequestDto){
+
+
+        return  null;
+    }
+
 }
--- master/src/main/java/com/foodFly/master/Model/Restaurant.java
+++ master/src/main/java/com/foodFly/master/Model/Restaurant.java
@@ -1,21 +1,29 @@
 package com.foodFly.master.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Restaurant {
 
+    @Id
     private Long restaurantId;
 
     private String mobileNumber;
     private String email;
     private String managerName;
 
-   private String password;
-   private String restaurantName;
-
-   private List<Address> addressesList;
-
-   private Enum Category;
+    private String password;
+    private String restaurantName;
+    private String Category;
 
 
 
