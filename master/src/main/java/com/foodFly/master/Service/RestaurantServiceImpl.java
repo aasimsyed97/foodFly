7b3b9b37575086c11929706c1f9f81cd5058a7f2
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-24 00:53:28 +0530

    Restaurant functionality added

--- master/src/main/java/com/foodFly/master/Service/RestaurantServiceImpl.java
+++ master/src/main/java/com/foodFly/master/Service/RestaurantServiceImpl.java
@@ -2,12 +2,14 @@ package com.foodFly.master.Service;
 
 import com.foodFly.master.DTOs.RestaurantRequestDto;
 import com.foodFly.master.DTOs.RestaurantResponseDto;
+import com.foodFly.master.Model.Restaurant;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RestaurantServiceImpl implements RestaurantService{
     @Override
     public RestaurantResponseDto registerRestaurant(RestaurantRequestDto restaurantRequestDto) {
+
         return null;
     }
 
@@ -25,4 +27,16 @@ public class RestaurantServiceImpl implements RestaurantService{
     public String deleteRestaurant(Long restaurantId) {
         return null;
     }
+
+     private Restaurant getRestaurant(RestaurantRequestDto restaurantRequestDto){
+
+        Restaurant restaurant = new Restaurant();
+        restaurant.setRestaurantName(restaurantRequestDto.getRestaurantName());
+        restaurant.setCategory(restaurantRequestDto.getCategory());
+        restaurant.setEmail(restaurantRequestDto.getEmail());
+        restaurant.setPassword(restaurantRequestDto.getPassword());
+        restaurant.setManagerName(restaurantRequestDto.getManagerName());
+        restaurant.setMobileNumber(restaurantRequestDto.getMobileNumber());
+        return restaurant;
+     }
 }
