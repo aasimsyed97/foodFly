3f70d3fc55aa33c6637bea51368cfb7f9fc46958
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-24 21:59:38 +0530

    Restaurant address  functionality added

--- master/src/main/java/com/foodFly/master/Service/RestaurantService.java
+++ master/src/main/java/com/foodFly/master/Service/RestaurantService.java
@@ -1,7 +1,9 @@
 package com.foodFly.master.Service;
 
+import com.foodFly.master.DTOs.AddressRequestDto;
 import com.foodFly.master.DTOs.RestaurantRequestDto;
 import com.foodFly.master.DTOs.RestaurantResponseDto;
+import com.foodFly.master.Model.Address;
 import com.foodFly.master.Model.Restaurant;
 
 public interface RestaurantService {
@@ -13,5 +15,7 @@ public interface RestaurantService {
     public Restaurant updateRestaurant(RestaurantRequestDto restaurantRequestDto);
 
     public String deleteRestaurant(Long restaurantId);
+
+    public Address updateRestaurantAddress(AddressRequestDto addressRequestDto);
 
 }
