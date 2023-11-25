29a5a7fcf1de495fc5740262cf26b3fe8b5948c2
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-26 02:10:39 +0530

    swagger dependency added

--- master/src/main/java/com/foodFly/master/MasterApplication.java
+++ master/src/main/java/com/foodFly/master/MasterApplication.java
@@ -2,12 +2,19 @@ package com.foodFly.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MasterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MasterApplication.class, args);
 	}
+
+
 
 }
