742332c51c478575800c6f8c15a954923647dd9d
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-26 17:42:00 +0530

    Item updated

---
+++ master/src/main/java/com/foodFly/master/Model/RestaurantItemMapping.java
@@ -0,0 +1,26 @@
+package com.foodFly.master.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestaurantItemMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    private Long itemId;
+
+    private Long restaurantId;
+
+}
