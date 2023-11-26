07a52b1d95a00ade29e9198e1e5dc5baa17a3ad8
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-26 18:41:58 +0530

    foodCart updated

---
+++ master/src/main/java/com/foodFly/master/Model/ItemCartMapping.java
@@ -0,0 +1,24 @@
+package com.foodFly.master.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemCartMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+    private Long itemId;
+    private Long FoodCartId;
+}
