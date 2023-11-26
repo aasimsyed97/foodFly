2114f5245573ca8e1a83b33f2b11051699f1b05e
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-26 17:41:02 +0530

    item updated

--- master/src/main/java/com/foodFly/master/Model/Item.java
+++ master/src/main/java/com/foodFly/master/Model/Item.java
@@ -1,14 +1,30 @@
 package com.foodFly.master.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long itemId;
 
     private Double cost;
     private String description;
     private String imgUrl;
     private String itemName;
-    private List<Restaurant> restaurantList;
+    private Enum Category;
+    private Enum Cuisine;
+
 }
