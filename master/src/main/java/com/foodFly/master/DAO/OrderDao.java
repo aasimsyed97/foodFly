b67e612ab1a920008f999564314edd0c58c2f9df
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-29 23:24:02 +0530

    Order functionality added.

---
+++ master/src/main/java/com/foodFly/master/DAO/OrderDao.java
@@ -0,0 +1,11 @@
+package com.foodFly.master.DAO;
+
+import com.foodFly.master.Model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderDao extends JpaRepository<Order, UUID> {
+}
