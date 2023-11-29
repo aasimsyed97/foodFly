b4ae28f75e1f272c402caed199d5fb55fd969b2d
aasimsyed97 <sayedaasim555@gmail.com>
2023-11-29 23:29:20 +0530

    Order functionality added.

---
+++ master/src/main/java/com/foodFly/master/Controller/OrderController.java
@@ -0,0 +1,9 @@
+package com.foodFly.master.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+
+}
---
+++ master/src/main/java/com/foodFly/master/Service/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package com.foodFly.master.Service;
+
+import com.foodFly.master.DAO.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    OrderDao orderDao;
+    @Autowired
+    FoodCartDao foodCartDao;
+
+    @Autowired
+    ItemDao itemDao;
+
+    @Autowired
+    RestaurantDao restaurantDao;
+    @Autowired
+    CustomerDao customerDao;
+
+
+
+}
